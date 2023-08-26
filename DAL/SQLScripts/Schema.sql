998f7b52b2104e616a7aa3522a06ef95d71ef991
Elamaran-MS <elamaran_ms@hotmail.com>
2023-08-26 12:10:52 +0530

    Add project files.

---
+++ DAL/SQLScripts/Schema.sql
@@ -0,0 +1,30 @@
+--CREATE TABLE dbo.Countries ( 
+--CountryId int IDENTITY(1,1) NOT NULL, 
+--Name varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL, 
+--CONSTRAINT PK_Countries PRIMARY KEY (CountryId) 
+--); 
+
+--INSERT INTO dbo.Countries (Name) VALUES('India'); 
+--INSERT INTO dbo.Countries (Name) VALUES('Srilanka'); 
+--INSERT INTO dbo.Countries (Name) VALUES('Pakistan');
+
+--CREATE TABLE dbo.Cities ( 
+--CityId int IDENTITY(1,1) NOT NULL, 
+--Name varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL, 
+--CountryId int NOT NULL, 
+--CONSTRAINT PK_City PRIMARY KEY (CityId), 
+--CONSTRAINT FK__City__CountryId FOREIGN KEY (CountryId) REFERENCES 
+--dbo.Countries(CountryId) 
+--);
+
+--INSERT INTO Cities (Name, CountryId) VALUES('New Delhi', 1); 
+--INSERT INTO Cities (Name, CountryId) VALUES('Mumbai', 1); 
+--INSERT INTO Cities (Name, CountryId) VALUES('Chennai', 1); 
+--INSERT INTO Cities (Name, CountryId) VALUES('Calcutta', 1); 
+--INSERT INTO Cities (Name, CountryId) VALUES('Lahore', 3); 
+--INSERT INTO Cities (Name, CountryId) VALUES('Colombo', 2); 
+--INSERT INTO Cities (Name, CountryId) VALUES('Kandy', 2); 
+--INSERT INTO Cities (Name, CountryId) VALUES('Jaipur', 1); 
+--INSERT INTO Cities (Name, CountryId) VALUES('Agra', 1); 
+--INSERT INTO Cities (Name, CountryId) VALUES('Indore', 1); 
+--INSERT INTO Cities (Name, CountryId) VALUES('Pune', 1); 
